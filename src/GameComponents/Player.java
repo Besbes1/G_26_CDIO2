27b6776e73fa59d38ff07db25f991c107d5988d3
s224329 <s224329@student.dtu.dk>
2022-10-18 16:26:07 +0200

    playerclass making

--- src/GameComponents/Player.java
+++ src/GameComponents/Player.java
@@ -1,4 +1,26 @@
 package GameComponents;
 
+
+//We are going to reuse the player class from the previous project CDIO1//
 public class Player {
+
+    private int playerNumber;
+    private int points;
+
+    public Player(int playerNumber, int points) {
+        this.playerNumber = playerNumber;
+        this.points = points;
+    }
+
+    public int getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public void setPoints(int newPoints) {
+        points = newPoints;
+    }
+
+    public int getPoints(){
+        return points;
+    }
 }
