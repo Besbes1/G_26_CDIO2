de35112fc400f46159e89434084a9d4a77fd640f
Poire Molle <s224311@dtu.dk>
2022-10-24 21:19:16 +0200

    Correcting the csv file path

--- src/Game.java
+++ src/Game.java
@@ -2,7 +2,7 @@ import GameComponents.*;
 import java.io.*;
 public class Game {
     public static void main(String[] args) {
-        String gameTextFile = "src\\gametext.csv";
+        String gameTextFile = "src/gametext.csv";
         BufferedReader csvReader;
         String line;
 
