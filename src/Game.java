ed03cb2746b6ecf3e6ba4a72be382a3aaa1b8516
Poire Molle <s224311@dtu.dk>
2022-10-24 23:34:55 +0200

    Making a getText method to initialize array from cvs and return String

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,11 @@
 import GameComponents.*;
+import jdk.jfr.Description;
+
 import java.io.*;
 public class Game {
     public static void main(String[] args) {
-
+        Text text = new Text();
+        System.out.println(text.getText(1));
 
     }
 }
--- src/GameComponents/Text.java
+++ src/GameComponents/Text.java
@@ -2,23 +2,23 @@ package GameComponents;
 import java.io.*;
 
 public class Text {
-
-    public void Reader() { String gameTextFile = "src/gametext.csv";
+    public String getText(int index) { String gameTextFile = "src/gametext.csv";
+        String[] row = new String[10];
         BufferedReader csvReader;
-        String line;
 
         try {
             csvReader = new BufferedReader(new FileReader(gameTextFile));
-            String[] row = new String[10];
             for(int i = 0 ; i < 10 ; i++) {
                 row[i] = csvReader.readLine();
 
             }
-            System.out.println(row[1]);
+
         }
         catch(Exception e) {
             e.printStackTrace();
 
         }
+
+        return row[index];
     }
 }
