ca2bf7f0a8efe2338f74102909088c95c9dc8655
Poire Molle <s224311@dtu.dk>
2022-10-24 21:16:33 +0200

    Splitting csv file into a String array

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,26 @@
-import java.util.Scanner;
 import GameComponents.*;
 import java.io.*;
 public class Game {
     public static void main(String[] args) {
+        String gameTextFile = "src\\gametext.csv";
+        BufferedReader csvReader;
+        String line;
 
+        try {
+            csvReader = new BufferedReader(new FileReader(gameTextFile));
+            while((line = csvReader.readLine()) != null) {
+                String[] row = line.split("\n");
+
+                for(String index : row) {
+                    System.out.printf(index);
+                }
+                System.out.println();
+            }
+
+        }
+        catch(Exception e) {
+
+        }
 
     }
 }
