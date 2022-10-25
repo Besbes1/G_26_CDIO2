5a484e9136197daa171cf31a0a2296415bf4e484
Mirzy12 <s205821@student.dtu.dk>
2022-10-25 10:39:24 +0200

    made account :)

--- src/GameComponents/Account.java
+++ src/GameComponents/Account.java
@@ -1,4 +1,28 @@
 package GameComponents;
 
 public class Account {
+    private double balance;
+    private String Accountname;
+
+    public Account(String name){
+        balance = 0;
+        Accountname = name;
+    }
+    public void setBalance(double initialBalance){
+        balance = initialBalance;
+    }
+    public void deposit(double amount)
+    {
+        balance += amount;
+    }
+    public void withdraw(double amount)
+    {
+        balance -= amount;
+    }
+    public void getBalance()
+    {
+        System.out.println(Accountname + "now has " + balance + " points");
+    }
 }
+
+
