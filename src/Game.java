98d342ddff1588758b4e8312aa1690b403e36d1f
Poire Molle <s224311@dtu.dk>
2022-10-25 17:02:31 +0200

    Added square description class

--- src/Game.java
+++ src/Game.java
@@ -4,9 +4,11 @@ import jdk.jfr.Description;
 import java.io.*;
 public class Game {
     public static void main(String[] args) {
-        Text text = new Text();
+        /*Text text = new Text();
         String welcomeMessage = text.getText(1);
-        System.out.println(welcomeMessage);
+        System.out.println(welcomeMessage);*/
+
+
 
     }
 }
--- src/GameComponents/Text.java
+++ src/GameComponents/Text.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 abstract class Text {
 
-    private String Read(String file) {
+    public String[] read(String file) {
         String gameTextFile = file;
         String[] row = new String[18];
         BufferedReader csvReader;
@@ -19,11 +19,43 @@ abstract class Text {
             e.printStackTrace();
 
         }
-        return row[index];
+        return row;
     }
 
-    public void printDescription(String sumDice) {
+}
 
+class SquareDescription extends Text{
+        String[]row=new String[18];
+
+        row=Read("src/squaredescriptions");
+        System.out.println(row[sumDice]);
+        }
+
+
+/*
+package GameComponents;
+import java.io.*;
+
+public class Text {
+    public String getText(int index) { String gameTextFile = "src/gametext.csv";
+        String[] row = new String[18];
+        BufferedReader csvReader;
+
+        try {
+            csvReader = new BufferedReader(new FileReader(gameTextFile));
+            for(int i = 0 ; i < 18 ; i++) {
+                row[i] = csvReader.readLine();
+
+            }
+
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+
+        }
+
+        return row[index];
     }
 }
 
+ */
