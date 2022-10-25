4ff77495a0391210207aa66655f32f9e447133d6
besbes <s224525@student.dtu.dk>
2022-10-25 12:05:53 +0200

    "added getter and array"

--- src/GameComponents/Square.java
+++ src/GameComponents/Square.java
@@ -1,4 +1,17 @@
 package GameComponents;
 
 public class Square {
+    // definere et array med alle første kast af terningen
+    int[] intarray;
+
+    public Square() {
+        intarray = new int[]{250, -100, 100, -20, 180, 0, -70, 60, -80,-50,650};
+    }
+
+    public int[] getPointValue() {
+        return intarray;
+    }
+
+
+
 }
