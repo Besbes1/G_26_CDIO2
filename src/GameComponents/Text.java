7b45fd7bffd993b9cc1864bbe0f7aebf5fc3ebea
Poire Molle <s224311@dtu.dk>
2022-10-25 16:31:51 +0200

    Made an abstract class for BufferedReader

--- src/GameComponents/Text.java
+++ src/GameComponents/Text.java
@@ -2,23 +2,23 @@ package GameComponents;
 import java.io.*;
 
 abstract class Text {
-    public String getText(int index) { String gameTextFile = "src/gametext.csv";
-        String[] row = new String[10];
+
+    public void Read(String file) {
+        String gameTextFile = file;
+        String[] row = new String[18];
         BufferedReader csvReader;
 
         try {
             csvReader = new BufferedReader(new FileReader(gameTextFile));
-            for(int i = 0 ; i < 10 ; i++) {
+            for (int i = 0; i < 18; i++) {
                 row[i] = csvReader.readLine();
 
             }
 
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
 
         }
-
-        return row[index];
     }
 }
+
