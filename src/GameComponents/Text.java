2c6f1acee6705a03aef76e82f28a7cb3a682c0ec
Poire Molle <s224311@dtu.dk>
2022-10-26 18:18:45 +0200

    Changing array size to 26

--- src/GameComponents/Text.java
+++ src/GameComponents/Text.java
@@ -5,14 +5,15 @@ public class Text {
     String file;
     public Text(String file){
         this.file = file;
+
     }
     public void printText(int index) {
-        String[] row = new String[18];
+        String[] row = new String[26];
         BufferedReader csvReader;
 
         try {
             csvReader = new BufferedReader(new FileReader(file));
-            for (int i = 0; i < 18; i++) {
+            for (int i = 0; i < 26; i++) {
                 row[i] = csvReader.readLine();
 
             }
