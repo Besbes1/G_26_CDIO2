88423436f911792d6a841af096438e4823a8338e
NegarOstad <s224283@student.dtu.dk>
2022-10-26 10:51:26 +0200

    Add a GUI object to the game klass

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,7 +1,12 @@
 import java.util.Scanner;
 import GameComponents.*;
+import gui_main.GUI;
+
 public class Game {
     public static void main(String[] args) {
+
+        GUI gui = new GUI();
+        gui.setDice(3,6);
 
     }
 }
