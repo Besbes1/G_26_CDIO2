cb79675c76e54e21a72502cec1c66bf8e438d539
Poire Molle <s224311@dtu.dk>
2022-10-26 19:01:32 +0200

    Updating pointArray in Square class to include new squares

--- src/GameComponents/Square.java
+++ src/GameComponents/Square.java
@@ -2,7 +2,7 @@ package GameComponents;
 
 public class Square {
     // definere et array med alle første kast af terningen
-    private int[] pointArray = {250, -100, 100, -20, 180, 0, -70, 60, -80,-50,650};
+    private int[] pointArray = {250, -100, 100, -20, 180, 0, -70, 60, -80, -50, 650, -500, 350, -700, 100, 75, 0, 140, 60, 50, 100, 40, 100};
 
 
     public int getPointValue(int diceSum) {
