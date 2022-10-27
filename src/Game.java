c5655abd89804ec87d3267f9748a7f7f709e188b
Poire Molle <s224311@dtu.dk>
2022-10-27 11:42:24 +0200

    Adding a do while statement to handle extra turn

--- src/Game.java
+++ src/Game.java
@@ -3,10 +3,49 @@ import java.util.Scanner;
 public class Game {
     public static void main(String[] args) {
         Text gameInstruc = new Text("src/gametext.csv");
+        Text squareDescrip = new Text("src/squaredescriptions");
+        Scanner input = new Scanner(System.in);
+        Player player1 = new Player(1,1000);
+        Player player2 = new Player(2,1000);
+        Cup diceCup = new Cup();
+        Square board = new Square();
+
+        int diceFaces;
+        int currentSum;
+        int pointsValue;
 
         //Prints welcome message
         gameInstruc.printText(1);
 
+        //Asks player to choose dice
+        gameInstruc.printText(7);
+
+        diceFaces = input.nextInt();
+        diceCup.setDiceFaces(diceFaces);
+
+        //Tells player which dice chosen
+        gameInstruc.printText(8);
+        System.out.println(diceFaces);
+
+        //while(player1)
+        boolean extraTurn;
+        do
+        {
+            extraTurn = true;
+            //THROWS THE DICE AND PRINTS SQUARE DESCRIPTION ACCORDINGLY
+            currentSum = diceCup.getSum();
+            squareDescrip.printText(currentSum);
+
+            //GETS THE VALUE OF THE SQUARE
+            pointsValue = board.getPointValue(currentSum);
+            player1.setPoints(pointsValue);
+            player1.printBalance();
+
+            if (currentSum != 10) {
+                extraTurn = false;
+            }
+
+        } while (extraTurn = true);
 
     }
 }
--- src/GameComponents/Account.java
+++ src/GameComponents/Account.java
@@ -1,29 +1,29 @@
 package GameComponents;
 
 public class Account {
-    private double balance;
+    private int balance;
     private String Accountname;
 
     public Account(String name){
         balance = 0;
         Accountname = name;
     }
-    public void setBalance(double initialBalance){
+    public void setBalance(int initialBalance){
         balance = initialBalance;
     }
-    public void deposit(double amount)
+    public void deposit(int amount)
     {
         balance += amount;
     }
-    public void withDraw(double amount)
+    public void withDraw(int amount)
     {
         balance -= amount;
         if (balance < 0)
             balance = 0;
     }
-    public void getBalance()
+    public int getBalance()
     {
-        System.out.println(Accountname + "now has " + balance + " points");
+        return balance;
     }
 }
 
