8520631704ab74f06a4dc032dcbb166cc0787af3
Poire Molle <s224311@dtu.dk>
2022-10-27 10:03:14 +0200

    Adding text in csv file for choosing the dice instructions

--- src/Game.java
+++ src/Game.java
@@ -1,15 +1,12 @@
 import GameComponents.*;
-
+import java.util.Scanner;
 public class Game {
     public static void main(String[] args) {
         Text gameInstruc = new Text("src/gametext.csv");
-        Text squareDescrip = new Text("src/squaredescriptions");
 
         //Prints welcome message
         gameInstruc.printText(1);
 
-        //Prints description of square 2
-        squareDescrip.printText(24);
 
     }
 }
