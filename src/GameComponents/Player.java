e8a83b614169adccc83ddcb0236cbccf452e560c
Poire Molle <s224311@dtu.dk>
2022-10-27 11:39:52 +0200

    Adding a printBalance method

--- src/GameComponents/Player.java
+++ src/GameComponents/Player.java
@@ -3,7 +3,7 @@ package GameComponents;
 
 //We are going to reuse the player class from the previous project CDIO1//
 public class Player {
-
+    Account bankAccount = new Account("Player Account");
     private int playerNumber;
     private int points;
 
@@ -18,10 +18,24 @@ public class Player {
     }
 
     public void setPoints(int newPoints) {
+
         points = newPoints;
+        if (points < 0){
+            bankAccount.withDraw(points);
+
+        } else {
+            bankAccount.deposit(points);
+        }
     }
 
-    public int getPoints(){
+    public int getBalance(){
+
         return points;
     }
+
+    public void printBalance() {
+        int newBalance = bankAccount.getBalance();
+        System.out.println("Player " + playerNumber + " now has " + newBalance + " in their bank account." );
+    }
 }
+
