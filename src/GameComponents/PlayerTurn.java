6ce96d1b01fd3d991396707e78c568df681658ee
Davidgtrivera <s215461@dtu.dk>
2022-10-27 17:26:43 +0200

    Modified: - Square class (Added method moveToSquare)
    Removed:
    - Switch statement from PlayerTurn

--- src/GameComponents/PlayerTurn.java
+++ src/GameComponents/PlayerTurn.java
@@ -3,63 +3,43 @@ package GameComponents;
 import java.util.Scanner;
 
 public class PlayerTurn {
-
+    // Square 2:    Deposit: 250
+    // Square 3:    Withdraw: 100
+    // Square 4:    Deposit: 100
+    // Square 5:    Withdraw: 20
+    // Square 6:    Deposit: 180
+    // Square 7:    Do nothing
+    // Square 8:    Withdraw: 70
+    // Square 9:    Deposit 60
+    // Square 10:   Withdraw: 80
+    // Square 11:   Withdraw: 50
+    // Square 12:   Deposit: 650
     public void turn(Player currentPlayer) {
 
-        Text squareDescription = new Text("src/squaredescriptions");
         Scanner userInput = new Scanner(System.in);
+        Square square = new Square();
         Cup Cup = new Cup();
         int Sum = 0;
+        int Point = 0;
 
         System.out.println(currentPlayer.getPlayerName() + " it is your turn");
         System.out.println("Press ENTER to roll the dice");
 
         userInput.nextLine();
 
-        Sum=Cup.getSum();
+        Sum = Cup.getSum();
+        Point = square.moveToSquare(Sum);
 
-        switch (Sum){
-            case 2:
-                squareDescription.printText(2);
-
-            case 3:
-                squareDescription.printText(3);
-
-            case 4:
-                squareDescription.printText(4);
-
-            case 5:
-                squareDescription.printText(5);
-
-            case 6:
-                squareDescription.printText(6);
-
-            case 7:
-                squareDescription.printText(7);
-
-            case 8:
-                squareDescription.printText(8);
-
-            case 9:
-                squareDescription.printText(9);
-
-            case 10:
-                squareDescription.printText(10);
-
-            case 11:
-                squareDescription.printText(11);
-
-            case 12:
-                squareDescription.printText(12);
-
-            default:
-                System.out.println("Error: Dice sum not within the range");
+        if (Point<0){
+            int newPoint = Math.abs(Point);
+            currentPlayer.takePoints(newPoint);
         }
-
-
-
-
-
+        else if (Point>0) {
+            currentPlayer.givePoints(Point);
+        }
+        else if (Point == 0){
+            System.out.println("You Didn't earn nor lose any points");
+        }
 
     }
 
--- src/GameComponents/Square.java
+++ src/GameComponents/Square.java
@@ -2,8 +2,19 @@ package GameComponents;
 
 public class Square {
     // definere et array med alle første kast af terningen
-    private int[] pointArray = {250, -100, 100, -20, 180, 0, -70, 60, -80,-50,650};
+    private int[] pointArray = {250, -100, 100, -20, 180, 0, -70, 60, -80, -50, 650};
 
+    // Square 2:    Deposit: 250
+    // Square 3:    Withdraw: 100
+    // Square 4:    Deposit: 100
+    // Square 5:    Withdraw: 20
+    // Square 6:    Deposit: 180
+    // Square 7:    Do nothing
+    // Square 8:    Withdraw: 70
+    // Square 9:    Deposit 60
+    // Square 10:   Withdraw: 80
+    // Square 11:   Withdraw: 50
+    // Square 12:   Deposit: 650
 
     public int getPointValue(int diceSum) {
 
@@ -11,4 +22,56 @@ public class Square {
         return i;
 
     }
+
+    public int moveToSquare(int Sum) {
+        Text squareDescription = new Text("src/squaredescriptions");
+        squareDescription.printText(Sum);
+        return (getPointValue(Sum));
+
+    }
 }
+
+
+      /*
+        switch (Sum){
+            case 2:
+                squareDescription.printText(2);
+                return (getPointValue(Sum));
+
+            case 3:
+                squareDescription.printText(3);
+                return (getPointValue(Sum));
+
+            case 4:
+                squareDescription.printText(4);
+                currentPlayer.givePoints(100);
+
+            case 5:
+                squareDescription.printText(5);
+
+
+            case 6:
+                squareDescription.printText(6);
+
+            case 7:
+                squareDescription.printText(7);
+
+            case 8:
+                squareDescription.printText(8);
+
+            case 9:
+                squareDescription.printText(9);
+
+            case 10:
+                squareDescription.printText(10);
+
+            case 11:
+                squareDescription.printText(11);
+
+            case 12:
+                squareDescription.printText(12);
+
+            default:
+                System.out.println("Error: Dice sum not within the range");
+
+         */
