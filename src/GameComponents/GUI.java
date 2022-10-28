e0353a71af1bdf79f943cba40fc599760a03e9f0
NegarOstad <s224283@student.dtu.dk>
2022-10-28 11:19:29 +0200

    Gui files

---
+++ src/GameComponents/GUI.java
@@ -0,0 +1,14 @@
+package GameComponents;
+
+public class GUI {
+
+    public static void addPlayer(String playerName, int playerNumber, int balance, int r, int g, int b){
+
+        GUI gui = new GUI();
+        final int maxPlayer = 6;
+
+        Player player = new Player(playerNumber, balance);
+
+    }
+
+}
